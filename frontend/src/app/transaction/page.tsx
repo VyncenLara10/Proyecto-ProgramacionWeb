9c2861e77b36eb711307f96c84febd5934fed274
CrisKroos25 <cristianguzman0928@gmail.com>
2025-10-05 14:44:02 -0600

    style(transaction): add styles for transaction cards and layout

--- frontend/src/app/transaction/page.tsx
+++ frontend/src/app/transaction/page.tsx
@@ -12,23 +12,24 @@ export default function TransactionPage() {
     <Layout>
       <TB />
 
-      <div className={styles.container}>
-        <h1>Transacciones</h1>
+      <div className={styles.container_main}>
+        <div className={styles.container}>
 
-        <div
-          className={styles.card}
-          onClick={() => router.push("/transaction/to-bank")}
-        >
-          <h2>Transferir a tu Banco</h2>
-          <p>Transfiere fondos desde tu cuenta Bancaria hacia Hapi</p>
-        </div>
-
-        <div
-          className={styles.card}
-          onClick={() => router.push("/transaction/to-hapi")}
-        >
-          <h2>Transferir a Hapi</h2>
-          <p>Transfiere fondos desde Hapi hacia tu cuenta bancaria</p>
+          <div
+            className={styles.card}
+            onClick={() => router.push("/transaction/to-bank")}
+          >
+            <h2>Transferir a tu Banco</h2>
+            <p>Transfiere fondos desde tu cuenta Bancaria hacia Hapi</p>
+          
+          </div>
+          <div
+            className={styles.card}
+            onClick={() => router.push("/transaction/to-hapi")}
+          >
+            <h2>Transferir a Hapi</h2>
+            <p>Transfiere fondos desde Hapi hacia tu cuenta bancaria</p>
+          </div>
         </div>
       </div>
     </Layout>
