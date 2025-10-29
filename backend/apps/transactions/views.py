83a7572920738e63234cea2ef3ed16fdfdb8848b
Roger Monterroso <rogerclad228@gmail.com>
2025-10-28 22:07:06 -0600

    feat(transactions): create a views and urls to transactions model

---
+++ backend/apps/transactions/views.py
@@ -0,0 +1,27 @@
+from rest_framework import generics, permissions
+from .models import Transaction
+from .serializers import TransactionSerializer
+
+class TransactionListView(generics.ListAPIView):
+    serializer_class = TransactionSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        # Show transactions
+        user = self.request.user
+        return Transaction.objects.filter(user=user).order_by("-timestamp")
+
+class TransactionCreateView(generics.CreateAPIView):
+    serializer_class = TransactionSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class TransactionDetailView(generics.RetrieveAPIView):
+    serializer_class = TransactionSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Transaction.objects.filter(user=user)
