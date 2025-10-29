4958363721d6eb8c39866f602e63c4c0b7cbf7f1
allanperez08 <allanperezajanel@gmail.com>
2025-10-28 22:15:33 -0600

    chore(admin_api): add app package initializer

--- backend/acciones/urls.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from .views import AccionViewSet
-
-router = DefaultRouter()
-router.register(r'acciones', AccionViewSet, basename='acciones')
-
-urlpatterns = router.urls
